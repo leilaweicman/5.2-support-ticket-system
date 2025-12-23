5b2a63aaee24985928895ebc6f214d85bf8e2fce
leilaweicman <leilaweic@hotmail.com>
2025-12-23 12:06:05 +0100

    chore: initial Spring Boot MVC project structure

---
+++ src/main/java/com/company/supportsystem/SupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.supportsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/supportsystem/SupportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.supportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
